682b215d4ec075d0aa2707536d8a7610f7051a38
zhangchuanchuan <zhangchuanchuan@58ganji.com>
2016-08-13 08:56:02 +0800

    添加我的

--- app/src/main/java/com/stream/nakedrent/fragment/MyFragment.java
+++ app/src/main/java/com/stream/nakedrent/fragment/MyFragment.java
@@ -1,5 +1,13 @@
 package com.stream.nakedrent.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.stream.nakedrent.R;
+
 /**
  * description：
  * ===============================
@@ -11,4 +19,9 @@ package com.stream.nakedrent.fragment;
  * Modify time：
  */
 public class MyFragment extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_myself, container, false);
+    }
 }
